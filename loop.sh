14a2089ca9a4fdc243afe909684c9f473739ca5e
aliceminotto <i.m.e@hotmail.it>
2016-05-06 12:21:47 +0100

    corrected synthax or at least i hope so

--- loop.sh
+++ loop.sh
@@ -16,7 +16,7 @@ do
         mkdir /$DST/velvet${i}_${j}_${k}_${l}
         cd /$DST/$FOLDER
         if [ ${k} -eq 0 ] && [ ${l} -eq 0 ];
-          then make "MAXKMERLENGTH=${i} CATEGORIES=${j}";
+          then make 'MAXKMERLENGTH=${i} CATEGORIES=${j}';
         elif [ ${k} -eq 1 ] && [ ${l} -eq 0 ];
           then make "MAXKMERLENGTH=${i} CATEGORIES=${j} BIGASSEMBLY=1";
         elif [ ${k} -eq 0 ] && [ ${l} -eq 1 ];
