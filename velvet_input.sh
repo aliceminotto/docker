4a0ce73f258d764bc81245a37711b135efaa0d16
Alice Minotto <i.m.e@hotmail.it>
2016-05-16 16:01:31 +0100

    Add files via upload

---
+++ velvet_input.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+MAINFLD=$1
+HASH=$2
+
+re='^[0-9]$'
+stringa=""
+
+if [[ $HASH =~ $re ]]
+  then 
+    MAINFLD='.'
+  else
+    kmers=(${HASH//,/ })
+    start=${kmers[0]}
+    end=${kmers[1]}
+    step=${kmers[2]}
+    for (( x=$start; x<$end; x+=$step ))
+      do
+        stringa+=$MAINFLD"_"$x", "
+      done
+fi
+
+echo $stringa
