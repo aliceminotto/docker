9c1e5894a6cee2bbecfb8f95fce83a930f8c3800
aliceminotto <i.m.e@hotmail.it>
2016-05-17 10:33:42 +0100

    fix mess with regular expression

--- velvet_input.sh
+++ velvet_input.sh
@@ -3,21 +3,22 @@
 MAINFLD=$1
 HASH=$2
 
-re='^[0-9]$'
+re='^[0-9]*$'
 stringa=""
 
 if [[ $HASH =~ $re ]]
-  then 
+  then
     stringa+=$MAINFLD
-  else
-    kmers=(${HASH//,/ })
-    start=${kmers[0]}
-    end=${kmers[1]}
-    step=${kmers[2]}
-    for (( x=$start; x<$end; x+=$step ))
-      do
-        stringa+=$MAINFLD"_"$x", "
-      done
+else {
+  kmers=(${HASH//,/ })
+  start=${kmers[0]}
+  end=${kmers[1]}
+  step=${kmers[2]}
+  for (( x=$start; x<$end; x+=$step ))
+    do
+      stringa+=$MAINFLD"_"$x", "
+    done
+  }
 fi
 
 echo $stringa
